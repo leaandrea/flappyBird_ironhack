8bc5ac2ea7095289c754188e13b6aa31f8cfc240
leaandrea <leaandreapaumier@gmail.com>
2019-06-13 17:59:16 +0200

    Added sprited, new logo, new flappy bird - now faby is crashing against a pipe and the game is reloading automatically

--- flappybird.js
+++ flappybird.js
@@ -1,37 +1,82 @@
-window.onload = function () {
-  document.getElementById("start-button").onclick = function () {
+window.onload = function() {
+  document.getElementById("start-button").onclick = function() {
     startGame();
   };
 
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
 
-  document.onkeydown = function (event) {
-    if (event.keyCode == 32) {
-      event.preventDefault();
-      Up();
-    }
-  };
-
   var img = new Image();
   img.src = "images/background.png";
   var imgX = 0;
   var imgY = 0;
 
-  img.onload = function () {
+  var isCrashed = false;
+
+  img.onload = function() {
     ctx.drawImage(img, 0, 0);
   };
 
   function drawBackground() {
     ctx.drawImage(img, 0, 0);
+    faby.y = faby.y + gravity;
     // ctx.drawImage(img, imgX + canvas.width, 0);
   }
 
-  var faby = new Image();
-  faby.src = "images/faby.png";
-  var fabyX = 130;
-  var fabyY = 190;
-  var gravity = 2;
+  var srcX;
+
+  var trackUp = 1;
+  var trackNone = 0;
+
+  var sheetWidth = 190;
+  var sheetHeight = 63;
+  var frameCount = 3;
+  var cols = 3;
+  var rows = 1;
+  var height = sheetHeight / rows;
+  const srcY = 0;
+  var width = sheetWidth / frameCount;
+  var currentFrame = 0;
+  var fabyImg = new Image();
+  fabyImg.src = "images/faby1.png";
+  fabyImg.onload = () => {
+    drawImage();
+  };
+
+  var moveUp = false;
+
+  function updateFrame() {
+    currentFrame = ++currentFrame % cols;
+    srcX = currentFrame * width;
+    //ctx.clearRect(faby.x, faby.y, width, height);
+  }
+
+  function drawImage() {
+    updateFrame();
+    ctx.drawImage(
+      fabyImg,
+      srcX,
+      srcY,
+      width,
+      height,
+      faby.x,
+      faby.y,
+      width,
+      height
+    );
+  }
+  // setInterval(function() {
+  //   drawImage();
+  // }, 500);
+
+  var faby = {
+    x: 130,
+    y: 190,
+    width: 61,
+    height: 53
+  };
+
+  var gravity = 1.5;
 
   // faby sounds
   var fly = new Audio();
@@ -43,18 +88,8 @@ window.onload = function () {
   var hit = new Audio();
   hit.src = "hitsound.wav";
 
-  faby.onload = function () {
-    ctx.drawImage(faby, 130, 190);
-  };
-
-  function drawFaby() {
-    ctx.drawImage(faby, fabyX, fabyY);
-    fabyY = fabyY + gravity;
-  }
-
   function Up() {
-    fabyY = fabyY - 45;
-    fly.play();
+    faby.y -= 40;
   }
 
   var pipes = [];
@@ -62,19 +97,16 @@ window.onload = function () {
     x: canvas.width,
     y: 0
   };
-
-  var spaceBetween = 100;
+  var spaceBetween = 112;
   var pipetop = new Image();
   var pipebottom = new Image();
   pipetop.src = "images/pipetop.png";
   pipebottom.src = "images/pipebottom.png";
 
-
-
   function Pipes() {
     for (var i = 0; i < pipes.length; i++) {
       pipes[i].x = pipes[i].x - 1.5; // vitesse
-      totalTop = pipetop.height + spaceBetween; // 
+      totalTop = pipetop.height + spaceBetween; //
       ctx.drawImage(pipetop, pipes[i].x, pipes[i].y);
       ctx.drawImage(pipebottom, pipes[i].x, pipes[i].y + totalTop);
       if (pipes[i].x === 370) {
@@ -84,35 +116,73 @@ window.onload = function () {
         });
       }
       if (
-        fabyX + faby.width > pipes[i].x &&
-        fabyX < pipes[i].x + pipetop.width &&
-        (fabyY < pipes[i].y + pipetop.height ||
-          fabyY + faby.height > pipes[i].y + totalTop) ||
-        fabyY + faby.height > canvas.height
+        (faby.x + faby.width > pipes[i].x &&
+          faby.x < pipes[i].x + pipetop.width &&
+          (faby.y < pipes[i].y + pipetop.height ||
+            faby.y + faby.height > pipes[i].y + totalTop)) ||
+        faby.y + faby.height > canvas.height
       ) {
-        // hit.play();
-        location.reload();
-        // reload page
+        isCrashed = true;
+        setInterval(() => location.reload(), 2000);
       }
-      if (pipes[i].x == 22) {
+      if ((pipes[i].x <= 80) & (pipes[i].x >= 60)) {
         // put score here maybe
         scorePoint.play();
+        // setTimeout(() => {
+        //   scorePoint.pause();
+        // }, 1000);
       }
     }
   }
 
+  // function gameOver() {
+  //   if (
+  //     (faby.x + faby.width > pipes[i].x &&
+  //       faby.x < pipes[i].x + pipetop.width &&
+  //       (faby.y < pipes[i].y + pipetop.height ||
+  //         faby.y + faby.height > pipes[i].y + totalTop)) ||
+  //     faby.y + faby.height > canvas.height
+  //   ) {
+  //   }
+  //   //   faby.y += 45;
+  //   // faby = 424;
+  // }
+
+  // // }
+  // setTimeout(() => (isCrashed = true), 200);
+  // document.getElementById(
+  //   "new-button"
+  // ).outerHTML = `<button id="start-button">Start New Game</button>`;
+  //   document.getElementById("getready").outerHTML = `
+  //   <div id="getready">
+  //   <img src="images/gameover.png" width="150px" height="50px">
+  // </div>
+
   function startGame() {
-    document.getElementById("start-button").outerHTML = "";
+    document.getElementById(
+      "start-button"
+    ).outerHTML = `<button id="new-button">Start New Game</button>`;
+    document.getElementById("getready").outerHTML = "";
     drawingLoop();
+    document.onkeydown = function(event) {
+      if (event.keyCode == 32) {
+        event.preventDefault();
+        Up();
+      }
+    };
   }
 
   function drawingLoop() {
-    ctx.clearRect(0, 0, 700, 500);
-    drawBackground();
-    drawFaby();
-    Pipes();
-    requestAnimationFrame(function () {
-      drawingLoop();
-    });
+    if (isCrashed === false) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      drawBackground();
+      drawImage();
+      Pipes();
+      requestAnimationFrame(function() {
+        drawingLoop();
+      });
+    }
   }
+
+  //setInterval(() => drawingLoop(), 50);
 };
